99e3059dda71720a58658dcdd2e2196337dcaa79
vladf <vlad.fox2002@mail.ru>
2024-11-21 11:55:09 +0300

    Добавил книги в таблицу

--- src/main/java/com/example/mrspobacked/configuration/DataConfiguration.java
+++ src/main/java/com/example/mrspobacked/configuration/DataConfiguration.java
@@ -1,6 +1,8 @@
 package com.example.mrspobacked.configuration;
 
+import com.example.mrspobacked.entities.BookEntity;
 import com.example.mrspobacked.entities.UserEntity;
+import com.example.mrspobacked.repositories.BookRepository;
 import com.example.mrspobacked.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
@@ -11,7 +13,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 public class DataConfiguration {
 
     @Bean
-    public CommandLineRunner initDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+    public CommandLineRunner initClientDatabase(UserRepository userRepository, PasswordEncoder passwordEncoder) {
         return args -> {
             if (!userRepository.existsByUsername("admin")) {
                 UserEntity admin = UserEntity.builder()
@@ -31,6 +33,34 @@ public class DataConfiguration {
                         .build();
                 userRepository.save(user);
             }
+        };
+    }
+
+    @Bean
+    public CommandLineRunner initBookDatabase(BookRepository bookRepository, PasswordEncoder passwordEncoder) {
+        return args -> {
+            BookEntity book1 = BookEntity.builder()
+                    .bookName("Гари Философский")
+                    .description("Очень крутая книга")
+                    .pageCount(300)
+                    .circulation(200)
+                    .authors("Крутой А.А::Забавный В.В")
+                    .genres("Триллер::Комедия")
+                    .rating(4.2)
+                    .onSale(true)
+                    .build();
+            BookEntity book2 = BookEntity.builder()
+                    .bookName("Саня с маслом")
+                    .description("Норм чтиво")
+                    .pageCount(123)
+                    .circulation(111)
+                    .authors("Веселый В.А:: Злой П.Р")
+                    .genres("Драмма::Комедия")
+                    .rating(4.1)
+                    .onSale(false)
+                    .build();
+            bookRepository.save(book1);
+            bookRepository.save(book2);
         };
     }
 }
